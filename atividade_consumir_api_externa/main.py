e53a8f4c11b4ed12133520c35171535e78a44f66
Eduardoooo0 <el957733@gmail.com>
2025-07-05 16:16:42 -0300

    adicionando atividade de consumir api do portal da transparencia

--- atividade_consumir_api_externa/main.py
+++ atividade_consumir_api_externa/main.py
@@ -1,20 +1,33 @@
-import requests
-import os
+from services import consultar_bolsa_familia_por_cpf_ou_nis, consultar_bolsa_familia_por_municipio, consultar_garantia_safra_por_cpf_ou_nis, consultar_seguro_defeso_por_cpf_ou_nis,consultar_servidor_do_poder_executivo_por_cpf
 
-headers = {
-     "chave-api-dados":os.getenv('TOKEN')
-}
+while True:
+    opcao = input('1 - Consultar bolsa família por CPF ou NIS\n2 - Consultar Bolsa Família por Municipio\n3 - Consultar Garantia-Safra por CPF ou NIS\n4 - Consultar Seguro Defeso por CPF ou NIS\n5 - Consultar Servidor do Poder Executivo por CPF\n6 - Sair\nDigite uma opção:')
+    if opcao == '1':     
+        cpf_ou_nis = input('Digite o CPF ou NIS:')
+        consultar_bolsa_familia_por_cpf_ou_nis(cpf_ou_nis)
 
-url = "https://api.portaldatransparencia.gov.br/api-de-dados/bolsa-familia-disponivel-por-cpf-ou-nis"
-cpf = input('CPF:')
+    elif opcao == '2':
+        ano = input('Digite o ano:')
+        mes = input('Digite o mês:')
+        codigo_ibge = input('Digite o código do municipio:')
+        ano_mes = ano+mes
+        consultar_bolsa_familia_por_municipio(ano_mes,codigo_ibge)
+    
+    elif opcao == '3':
+        cpf_ou_nis = input('Digite o CPF ou NIS:')
+        consultar_garantia_safra_por_cpf_ou_nis(cpf_ou_nis)
 
-params = {
-    "codigo":cpf,
-    'anoMesReferencia':202401,
-    'anoMesCompetencia':202501,
-    'pagina':1
-}
+    elif opcao == '4':
+        cpf_ou_nis = input('Digite o CPF ou NIS:')
+        consultar_seguro_defeso_por_cpf_ou_nis(cpf_ou_nis)
+    
+    elif opcao == '5':
+        cpf = input('Digite o CPF:')
+        consultar_servidor_do_poder_executivo_por_cpf(cpf)
 
-r = requests.get(url,params=params ,headers=headers)
+    elif opcao == '6':
+        print('finalizando...')
+        break
 
-print(r.status_code)
+    else:
+        pass
